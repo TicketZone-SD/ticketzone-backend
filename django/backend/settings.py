5687e25bdd21b2e3660ca7a8211a9c54996ee513
tamiressa <4serimat@gmail.com>
2025-02-24 22:49:45 -0300

    'migrations'

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -23,6 +23,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'backend',
+    
 ]
 
 # Middlewares
@@ -83,3 +86,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Tipo padrão de chave primária
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
